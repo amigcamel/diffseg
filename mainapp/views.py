d4e1dab17f6c0aabf7ba8d6b382e973ff64030bb
amigcamel <amigcamel@gmail.com>
2016-06-09 17:18:21 +0800

    add pyquery

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,17 @@
+import requests
+from pyquery import PyQuery as Pq
 from django.shortcuts import render
 
-# Create your views here.
+
+def livac(text):
+    url = 'http://www.livac.org/seg/livac_seg_index.php?lang=tc'
+    data = {
+        'action': 'search',
+        'lang': 'tc',
+        'search': text
+    }
+    resp = requests.post(url, data=data)
+    return resp
+    dom = Pq(resp.raw)
+    res = dom('#segmentResult')
+    return res
